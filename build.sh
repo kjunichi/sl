fb5475220e88cfb61d3f62e32df8557736637950
Junichi Kajiwara <junichi.kajiwara@gmail.com>
2014-11-05 13:19:54 +0900

    update

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 set -e
-EM_DIR=~/local/emscripten
-$EM_DIR/emcc -O2 -Wall -fPIC -I. -DEMSCRIPTEN -o sl.html sl.c libpdcurses.a `sdl-config --libs` --preload-file pdcfont.bmp
+$EMSCRIPTEN/emcc -s ASYNCIFY=1 -O2 -Wall -fPIC -I. -DEMSCRIPTEN -o sl.html sl.c libpdcurses.a `sdl-config --libs` --preload-file pdcfont.bmp
