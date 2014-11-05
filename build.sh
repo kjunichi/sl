6146bbfef8780e0d20ee03c627501eb7db09f65f
Junichi Kajiwara <junichi.kajiwara@gmail.com>
2014-11-05 13:17:08 +0900

    Revert "update"
    This reverts commit a239184ced2b85f0c900a7a20e94aa393b236313.

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 set -e
-$EMSCRIPTEN/emcc -s ASYNCIFY=1 -O2 -Wall -fPIC -I. -DEMSCRIPTEN -o sl.html sl.c libpdcurses.a `sdl-config --libs` --preload-file pdcfont.bmp
+EM_DIR=~/local/emscripten
+$EM_DIR/emcc -O2 -Wall -fPIC -I. -DEMSCRIPTEN -o sl.html sl.c libpdcurses.a `sdl-config --libs` --preload-file pdcfont.bmp
